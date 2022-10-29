50379713298ad003d350e68441e27ef50f320cb9
fearsd <rifatfazlutdinov@gmail.com>
2022-10-29 19:49:54 +0300

    adds table for OKS

--- backend/db/models/oks.py
+++ backend/db/models/oks.py
@@ -5,14 +5,16 @@ from sqlalchemy.dialects.postgresql import ARRAY
 from backend import db
 
 
-
-
-class Land(db.Model):
-    __tablename__ = "lands" # земельные участки
+class ConstructionWorks(db.Model):
+    __tablename__ = "OKS" # ОКС (объект капитального строительства)
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-
     oid = Column(Integer, nullable=True)
     shapetype = Column(Integer, Enum(shapefile.SHAPETYPE_LOOKUP))
     parts = Column(ARRAY(Integer))
     points = Column(ARRAY(DECIMAL()))
+    bbox = Column(ARRAY(DECIMAL()))
+
+    cadnum = Column(String())
+    address = Column(String())
+    area = Column(DECIMAL())
