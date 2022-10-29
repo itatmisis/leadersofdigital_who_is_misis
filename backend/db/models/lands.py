e741d50622e120aaaa7835034841984081462c7f
sssemion <ssemi.on@yandex.ru>
2022-10-29 19:03:57 +0300

    ЗУ модель

--- backend/db/models/lands.py
+++ backend/db/models/lands.py
@@ -5,14 +5,19 @@ from sqlalchemy.dialects.postgresql import ARRAY
 from backend import db
 
 
-
-
 class Land(db.Model):
     __tablename__ = "lands"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
 
-    oid = Column(Integer, nullable=True)
+    oid = Column(Integer)
     shapetype = Column(Integer, Enum(shapefile.SHAPETYPE_LOOKUP))
     parts = Column(ARRAY(Integer))
-    points = Column(ARRAY(DECIMAL()))
+    points = Column(ARRAY(DECIMAL(19)))
+    bbox = Column(ARRAY(DECIMAL(19)))
+
+    cadnum = Column(String(80))
+    address = Column(String(255))
+    has_effect = Column(Integer)
+    property_t = Column(Integer)
+    shape_area = Column(DECIMAL(19))
