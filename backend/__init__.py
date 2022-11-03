acb57bfbc9398c8ade10791137746c43ca2e66cf
sssemion <ssemi.on@yandex.ru>
2022-11-03 22:34:30 +0300

    cors

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,13 +4,17 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-
 dotenv_path = os.path.join(os.path.dirname(__file__), "..", ".env")
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 else:
     raise FileNotFoundError(".env file not found")
+
+app = Flask(__name__)
+
+if os.environ.get("DEBUG") == "true":
+    import flask_cors
+    cors = flask_cors.CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
