7682d29bd8cb746fef822f5e5003c74f02269330
Gennady Alheev <44393437+MiiMan@users.noreply.github.com>
2022-11-03 04:18:16 +0300

    add: google fonts

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/presentation/screens/main_screen/main_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: GoogleFonts.inter().fontFamily
       ),
       debugShowCheckedModeBanner: false,
       home: const MainScreen(),
--- frontend/lib/presentation/theme/app_fonts.dart
+++ frontend/lib/presentation/theme/app_fonts.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 extension LogicSize on num {
   static const double _scaleFactor = 0.8;
