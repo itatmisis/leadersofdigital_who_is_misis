b696f18040674c4d775d5d4d616f9662389d62b0
Gennady Alheev <44393437+MiiMan@users.noreply.github.com>
2022-11-05 01:28:47 +0300

    add: ShortMenu

---
+++ frontend/lib/presentation/screens/main_screen/widgets/short_menu.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ShortMenu extends StatelessWidget {
+  const ShortMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Text('sdsdsdsd')
+          ],
+        ),
+      ),
+    );
+  }
+
+}
