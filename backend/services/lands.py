13ce65eaef59f33c6ba2c39d44b827e6f0cfca26
sssemion <ssemi.on@yandex.ru>
2022-11-06 21:40:11 +0300

    bbox fix fix fix fix fix

--- backend/services/lands.py
+++ backend/services/lands.py
@@ -4,6 +4,6 @@ from backend.tools.bbox import Bbox
 
 
 def select_all_in_bbox(bbox: Bbox):
-    query = db.session.execute(f"select * from lands where st_intersects(points, st_makevalid('{wkt.dumps(bbox.polygon)})'))").all()
+    query = db.session.execute(f"select * from lands where st_intersects(points, st_makevalid('{wkt.dumps(bbox.polygon)}'))").all()
 
     return query
