fa3e6cfcd8c7911cb5a8fb33c3a01f9cdf1bad22
sssemion <ssemi.on@yandex.ru>
2022-11-06 21:13:19 +0300

    select bbox

--- backend/services/lands.py
+++ backend/services/lands.py
@@ -1,17 +1,9 @@
-from backend.data.models import Land, ExtendedLand
-import shapely.geometry
-import geoalchemy2.functions
 from shapely import wkt
 from backend import db
 from backend.tools.bbox import Bbox
 
 
 def select_all_in_bbox(bbox: Bbox):
-    query = db.session\
-        .query(ExtendedLand)\
-        .filter(geoalchemy2.functions.ST_Intersects(ExtendedLand.land.points, wkt.dumps(bbox.polygon)))\
-        .all()
+    query = db.session.execute(f"select * from lands where st_intersects(points, {wkt.dumps(bbox.polygon)})").all()
 
     return query
-
-
